9a1121f38f7ea418a363376ff8ea156bcd780e00
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-02-11 20:54:25 +0900

    write page view 완성

--- src/pages/DiaryWrite.js
+++ src/pages/DiaryWrite.js
@@ -16,12 +16,45 @@ function DiaryWrite() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    const titleRef = React.useRef(null);
+    const ImageRef = React.useRef(null);
+    const contentsRef = React.useRef(null);
+    const tagRef = React.useRef(null);
+
+    const [title,setTitle] = React.useState("");
+    const [Image_url,setImage_url] = React.useState("");
+    const [contents,setContents] = React.useState("");
+
+    const changeImg_url = () => {
+        console.log(ImageRef.current.value)
+        setImage_url(ImageRef.current.value);
+    }
+    
+    const sendData = () =>{
+        console.log(titleRef.current.value)
+        console.log(ImageRef.current.value)
+        console.log(contentsRef.current.value)
+        console.log(tagRef.current.value)
+    }
+
     React.useEffect(async() => {
     },[]);
 
     return (
+        <Grid>
+            <Header/>
+                <Grid is_flex flex_direction="column" align-items="center">
+                    <Input _ref={titleRef} margin="10px" label="Title" width = "30vw"></Input>
+                    <Text margin="20px">미리보기</Text>
+                    <Image margin="20px" width="100%" src={Image_url?Image_url:"https://t1.daumcdn.net/cfile/tistory/206CA00E4CF0B11229"}/>
+                    <Grid margin="10px"/>
+                    <Input _ref={ImageRef} margin="10px" label="Image_url" width = "30vw" _onChange={changeImg_url}></Input>
+                    <Input _ref={contentsRef} margin="10px" label="Contents" width = "30vw"></Input>
+                    <Input _ref={tagRef} margin="10px" label="Hash tag" width = "30vw"></Input>
+                    <Button margin="20px" width="15vw" text="Signup" _onClick={sendData} />
 
-        <div> it's DiaryWrite</div>
+                </Grid>
+        </Grid>
 
     )
 
