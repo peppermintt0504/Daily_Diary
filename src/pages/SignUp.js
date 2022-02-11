396577ab2dc93d0301daecaead59fd8cabf155d2
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-02-11 20:26:02 +0900

    Update SignUp.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -16,6 +16,34 @@ function SignUp() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    const idRef = React.useRef(null);
+    const nickRef = React.useRef(null);
+    const pwdRef = React.useRef(null);
+    const pwdCheckRef = React.useRef(null);
+
+    const [id,setId] = React.useState("");
+    const [nick,setNick] = React.useState("");
+    const [pwd,setPwd] = React.useState("");
+
+
+    const sendData = () =>{
+        console.log(idRef.current.value)
+        console.log(nickRef.current.value)
+        console.log(pwdRef.current.value)
+        console.log(pwdCheckRef.current.value)
+
+        
+
+    }
+
+    const pwdCheck = () =>{
+        if(pwdRef.current.value !== pwdCheckRef.current.value){
+            console.log("plz recheck your password...");
+        }else{
+            console.log("currect pwd...");
+        }
+    }
+
 
     React.useEffect(async() => {
     },[]);
@@ -24,11 +52,14 @@ function SignUp() {
         <Grid>
             <Header/>
             <Grid is_flex flex_direction="column" align-items="center">
-                <Input margin="10px" label="ID" width = "30vw"></Input>
-                <Input margin="10px" label="NickName" width = "30vw"></Input>
-                <Input margin="10px" label="PassWord" width = "30vw"></Input>
-                <Input margin="10px" label="CheckPassWord" width = "30vw"></Input>
-                <Button margin="20px" width="15vw" text="Signup" />
+                <Grid justify_content="center" is_flex align-items="center">
+                <Input _ref={idRef} margin="10px" label="ID" width = "25vw"></Input>
+                <Button margin="15px 0 0 0" text="중복체크" width="5vw" height="50px" _onClick={pwdCheck}/>
+                </Grid>
+                <Input _ref={nickRef} margin="10px" label="NickName" width = "30vw"></Input>
+                <Input _ref={pwdRef} margin="10px" label="PassWord" width = "30vw"></Input>
+                <Input _ref={pwdCheckRef} margin="10px" label="CheckPassWord" width = "30vw"></Input>
+                <Button margin="20px" width="15vw" text="Signup" _onClick={sendData} />
             </Grid>
         </Grid>
     )
