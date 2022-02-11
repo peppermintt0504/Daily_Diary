bbf8a312b10a23c7fe4cb4d0d6f5c5abaaf4ca7d
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-02-11 20:29:30 +0900

    update write page

--- src/pages/DiaryWrite.js
+++ src/pages/DiaryWrite.js
@@ -4,26 +4,27 @@ import styled from "styled-components";
 import {useDispatch, useSelector} from "react-redux";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
-//import Components
+//import Components && elements
+import Header from "../components/Header";
 import { Button, Grid, Input, Image, Text } from "../elements" 
 
-//import Icon
-import { BsFillMenuButtonWideFill } from "react-icons/bs";
-import Header from "../components/Header";
+//import API
+import {RESP} from "../shared/tempAPI"
 
-function Temp() {
+
+function DiaryWrite() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     React.useEffect(async() => {
     },[]);
 
     return (
 
-        <Grid >
-            <Header/>
-        
-        </Grid>
+        <div> it's DiaryWrite</div>
 
-    );
+    )
+
 }
 
-export default Temp;
+export default DiaryWrite;
